408ba7e6284a90c1bc6ceb9df7e49478fdcf8046
Prem Jatol <140836410+SmartlyPrem@users.noreply.github.com>
2024-04-04 15:23:32 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import RoutingApp from './routing/RoutingApp.jsx';
 import Index from './Context/Index.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -10,5 +9,4 @@ root.render(
         <Index>
                 <App />
         </Index>
-        // <RoutingApp/>
 );
